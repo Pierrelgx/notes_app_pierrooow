50d0f38377746b9e7069cc5bc1cd2841bdb09495
PierreLgx <97539842+Pierrooow@users.noreply.github.com>
2022-05-13 07:47:38 +0200

    first commit

--- src/Main.js
+++ src/Main.js
@@ -1,41 +1,42 @@
+import ReactMarkdown from "react-markdown";
+
 function Main({ activeNote, onUpdateNote }) {
+  const onEditField = (key, value) => {
+    onUpdateNote({
+      ...activeNote,  
+      [key]: value,
+      lastModified: Date.now(),
+    });
+  };
 
-    const onEditField = (key, value) => {
-        onUpdateNote({
-            id: activeNote.id, 
-            [key]: value,
-            lastModified: Date.now(),
-        })
+  if(!activeNote) return <div className="no-active-note">No note selected</div>
 
-    };
+  return (
+    <div className="app-main">
+      <div className="app-main-note-edit">
+        <input 
+          type="text" 
+          id="tittle" 
+          value={activeNote.title} 
+          onChange={(e) =>onEditField("title", e.target.value)} 
+          autoFocus 
+        />
+        <textarea 
+          id="body" 
+          placeholder="Écrivez votre note ici..." 
+          value={activeNote.body} 
+          onChange={(e) =>onEditField("body", e.target.value)}
+        />
 
-if(!activeNote) return <div className="no-active-note">No Note selected</div>
-
-    return <div className="app-main">
-
-        <div className="app-main-note-edit">
-        <input type="text" id="title" value={activeNote.title} 
-        onChange={(e) => onEditField("title", e.target.value )} 
-        autoFocus />
-        <textarea id="body" placeholder="Write your note here babyyyy" value={activeNote.body} />
-        </div>
-
-        <div className="app-main-note-preview">
-        <h1 className="preview-title" >{activeNote.title}</h1>
-            <div className="markdown-preview">{activeNote.body}
-            onChange={(e) => onEditField("body", e.target.value )} 
-
-            </div>
-
-
-        </div>
-
-
-
-
+      </div>
+      <div className="app-main-note-preview">
+        <h1 className="preview-title">{activeNote.title}</h1>
+        <ReactMarkdown className="markdown-preview">
+          {activeNote.body}
+        </ReactMarkdown>
+      </div>
     </div>
-
-
+  );
 }
 
-export default  Main;
+export default Main; 
